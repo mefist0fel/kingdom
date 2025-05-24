a31756805a5f6b2000994612a59d230a8f72e5e6
Mefistofel <mfstfl@gmail.com>
2025-05-24 23:50:35 +0200

    Added grid generation

--- src/game_state.c
+++ src/game_state.c
@@ -1,20 +1,22 @@
 #include "pd_api.h"
 #include "state.h"
+#include "grid.h"
 #include "assets.h"
+
+#ifdef _WINDLL
+__declspec(dllexport)
+#endif
 
 #define max(a,b) \
   ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
 #define min(a,b) \
   ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 
-
-#ifdef _WINDLL
-__declspec(dllexport)
-#endif
-
 extern void CreateMenuState();
 
 typedef struct {
+    Grid* grid;
+
     int x;
     int y;
     int offset_x;
@@ -25,9 +27,70 @@ typedef struct {
     LCDBitmap* castle;
 } GameData;
 
-#define CURSOR_SPEED 4
+#define LCG_A 1664525
+#define LCG_C 1013904223
+#define LCG_M 0xFFFFFFFF
+
+static unsigned int lcg_next(unsigned int* seed) {
+    *seed = (*seed) * LCG_A + LCG_C; // & LCG_M;
+    return *seed;
+}
+
+Grid* createGrid(int map_x, int map_y) {
+    Grid* grid = pd->system->realloc(NULL, sizeof(Grid));
+    grid->data = pd->system->realloc(NULL, sizeof(int) * map_x * map_y);
+    grid->size_x = map_x;
+    grid->size_y = map_y;
+    for (int i = 0; i < map_x * map_y; i++) {
+        grid->data[i] = 0;
+    }
+    return grid;
+}
+
+Grid* createRandomGrid(int map_x, int map_y) {
+    Grid* grid = pd->system->realloc(NULL, sizeof(Grid));
+    grid->size_x = map_x;
+    grid->size_y = map_y;
+    grid->data = pd->system->realloc(NULL, sizeof(int) * map_x * map_y);
+
+    unsigned int seed = 0;
+    for (int i = 0; i < map_x * map_y; i++) {
+        grid->data[i] = (int)lcg_next(&seed) % 3;
+    }
+
+    return grid;
+}
+
+int getGridValue(const Grid* grid, int x, int y) {
+    if (!grid || x < 0 || y < 0 || x >= grid->size_x || y >= grid->size_y)
+        return -1; // error
+    return grid->data[y * grid->size_x + x];
+}
+
+void setGridValue(const Grid* grid, int x, int y, int value) {
+    if (!grid || x < 0 || y < 0 || x >= grid->size_x || y >= grid->size_y)
+        return;
+    grid->data[y * grid->size_x + x] = value;
+}
+
+void destroyGrid(Grid* grid) {
+    if (!grid) return;
+    pd->system->realloc(grid->data, 0);
+    pd->system->realloc(grid, 0);
+}
+
+
+
+#define CURSOR_SPEED 8
 #define TILE_X 32
 #define TILE_Y 32
+
+#define CURSOR_X 32
+#define CURSOR_Y 32
+
+#define CANVAS_X 400
+#define CANVAS_Y 240
+
 int rx;
 int ry;
 
@@ -36,23 +99,31 @@ static void GameUpdateTyped(GameData* g) {
     PDButtons current;
     pd->system->getButtonState(&current, &pushed, NULL);
 
-    if (current & kButtonDown)
-        g->y += 1;
-    if (current & kButtonUp)
-        g->y -= 1;
     if (current & kButtonLeft)
-        g->x -= 1;
+        g->x -= CURSOR_SPEED;
     if (current & kButtonRight)
-        g->x += 1;
+        g->x += CURSOR_SPEED;
+    if (current & kButtonDown)
+        g->y += CURSOR_SPEED;
+    if (current & kButtonUp)
+        g->y -= CURSOR_SPEED;
 
-    if (current & kButtonDown)
-        g->offset_y += CURSOR_SPEED;
-    if (current & kButtonUp)
-        g->offset_y -= CURSOR_SPEED;
-    if (current & kButtonLeft)
+    if (g->x < 0) {
+        g->x += CURSOR_SPEED;
         g->offset_x -= CURSOR_SPEED;
-    if (current & kButtonRight)
+    }
+    if (g->x > CANVAS_X - CURSOR_X) {
+        g->x -= CURSOR_SPEED;
         g->offset_x += CURSOR_SPEED;
+    }
+    if (g->y < 0) {
+        g->y += CURSOR_SPEED;
+        g->offset_y -= CURSOR_SPEED;
+    }
+    if (g->y > CANVAS_Y - CURSOR_Y) {
+        g->y -= CURSOR_SPEED;
+        g->offset_y += CURSOR_SPEED;
+    }
 
     if (pushed & kButtonA) {
         CreateMenuState();
@@ -69,7 +140,22 @@ static void GameUpdateTyped(GameData* g) {
     int startOffsetY = g->offset_y - tileOffsetY * TILE_Y;
 
 
-    for (int y = 0; y < 8; ++y)
+    for (int y = 0; y < 9; ++y)
+    {
+        for (int x = 0; x < 14; ++x)
+        {
+            rx = x + tileOffsetX;
+            ry = y + tileOffsetY;
+            if (rx < 0 || rx >= g->grid->size_x)
+                continue;
+            if (ry < 0 || ry >= g->grid->size_y)
+                continue;
+
+            LCDBitmap* tile = (getGridValue(g->grid, rx, ry) == 0) ? g->forest : g->grass;
+            pd->graphics->drawBitmap(tile, -startOffsetX + x * 32, -startOffsetY + y * 32 - (rx % 2) * 16, kBitmapUnflipped);
+        }
+    }
+    for (int y = 0; y < 9; ++y)
     {
         for (int x = 0; x < 14; ++x)
         {
@@ -80,12 +166,13 @@ static void GameUpdateTyped(GameData* g) {
             if (ry < 0 || ry >= mapY)
                 continue;
 
-            LCDBitmap* tile = ((rx + ry) % 13 == 0) ? g->forest : g->grass;
-            pd->graphics->drawBitmap(tile, -startOffsetX + x * 32, -startOffsetY + y * 32 - (rx % 2) * 16, kBitmapUnflipped);
+            if ((rx + 3 + ry) % 19 == 0) {
+                pd->graphics->drawBitmap(g->castle, -startOffsetX + x * 32 - 16, -startOffsetY + y * 32 - (rx % 2) * 16 - 32, kBitmapUnflipped);
+            }
         }
     }
     
-    pd->graphics->drawBitmap(g->castle, 200, 100, kBitmapUnflipped);
+    //pd->graphics->drawBitmap(g->castle, 200, 100, kBitmapUnflipped);
 
     pd->graphics->drawBitmap(g->cursor, g->x, g->y, kBitmapUnflipped);
 }
@@ -97,6 +184,7 @@ static void GameUpdate(void* ptr) {
 static void GameExit(void* ptr) {
     GameData* g = (GameData*)ptr;
 
+    destroyGrid(g->grid);
     pd->graphics->freeBitmap(g->grass);
     pd->graphics->freeBitmap(g->forest);
     pd->graphics->freeBitmap(g->castle);
@@ -109,17 +197,18 @@ void CreateGameState() {
     GameData* g = pd->system->realloc(NULL, sizeof(GameData));
     const char* err;
 
+    g->grid = createRandomGrid(32, 32);
     g->grass = pd->graphics->loadBitmap(IMG_TILE_GRASS, &err);
     g->forest = pd->graphics->loadBitmap(IMG_TILE_TREE, &err);
     g->castle = pd->graphics->loadBitmap(IMG_CASTLE, &err);
 
     g->cursor = pd->graphics->loadBitmap(IMG_CURSOR, &err);
 
-    g->x = 200;
-    g->y = 100;
+    g->x = 200 - 16;
+    g->y = 120 - 16;
 
-    g->offset_x = -100;
-    g->offset_y = -50;
+    g->offset_x = 0;
+    g->offset_y = 0;
 
     switchState(g, NULL, GameUpdate, GameExit);
 }
---
+++ src/grid.h
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef struct {
+    int* data;
+    int size_x;
+    int size_y;
+} Grid;
